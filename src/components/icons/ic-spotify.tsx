77eb93769afe5f97a3ed0dd4bd6c7ea4198c65ec
Yoga Permana <yoga@brandztory.id>
2023-09-04 17:34:39 +0700

    initial prod

---
+++ src/components/icons/ic-spotify.tsx
@@ -0,0 +1,24 @@
+import type { SVGProps } from "react";
+
+export function IcSpotify(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      >
+        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0-18 0"></path>
+        <path d="M8 11.973c2.5-1.473 5.5-.973 7.5.527M9 15c1.5-1 4-1 5 .5M7 9c2-1 6-2 10 .5"></path>
+      </g>
+    </svg>
+  );
+}
--- src/components/sites/profile.tsx
+++ src/components/sites/profile.tsx
@@ -1,21 +1,39 @@
+import { IcGithub } from "../icons/ic-github";
 import { IcInstagram } from "../icons/ic-instagram";
+import { IcSpotify } from "../icons/ic-spotify";
 import { IcX } from "../icons/ic-x";
 
 export default function Profile() {
   return (
     <section className="container mx-auto py-4 px-4 lg:px-0">
       <div className="typography">
-        <h1>Hello World</h1>
-        <p>Hi! I am Yoga Permana.</p>
+        <h1 className="py-12">Hello World</h1>
+        <p>
+          Hi! I am <span className="text-jade-11 font-bold">Yoga Permana.</span>
+        </p>
 
         <p>
-          Working at <span className="text-orange-9">BrandZtory.</span>
+          Working as a{" "}
+          <span className="text-jade-11 font-bold">Fullstack Developer.</span>
         </p>
 
         <p>Find me on:</p>
         <div className="py-4 flex gap-4">
-          <IcInstagram className="w-6 h-6" />
-          <IcX className="w-6 h-6" />
+          <a href="https://www.instagram.com/yopernurr" target="_blank">
+            <IcInstagram className="w-6 h-6" />
+          </a>
+          {/* <a href="https://www.instagram.com/yopernurr" target="_blank">
+            <IcX className="w-6 h-6" />
+          </a> */}
+          <a href="https://www.github.com/stooezy" target="_blank">
+            <IcGithub className="w-6 h-6" />
+          </a>
+          <a
+            href="https://open.spotify.com/user/yogapermana30?si=070088b62c134299"
+            target="_blank"
+          >
+            <IcSpotify className="w-6 h-6" />
+          </a>
         </div>
       </div>
     </section>
